8a4773d67ea61798abd67e39a753380086061966
Vitalii <vedmidu4asi@gmail.com>
2023-09-30 18:48:29 -0400

    Player collision

--- smashdungeon/Assets/Scripts/Player.cs
+++ smashdungeon/Assets/Scripts/Player.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 class Player: MonoBehaviour {
 
+    public float speed = 10f;
+    
     private void Start() {
     }
 
     private void Update() {
+    }
+
+    private void OnMove(InputValue input) {
+        var velocity = input.Get<Vector2>();
+        if (velocity != Vector2.zero)
+            GetComponent<Rigidbody2D>().velocity = velocity * speed;
     }
 
 }
