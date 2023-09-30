7991ad2c73bb7b932bbd40bbc2522f8c58693677
humancoded <139584104+humancoded@users.noreply.github.com>
2023-09-30 17:52:36 -0400

    walls scaling

---
+++ smashdungeon/Assets/Scripts/Base.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    public Transform topWall;
+    public Transform bottomWall;
+    public Transform leftWall;
+    public Transform rightWall;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var scaleIncrease = new Vector3(Time.deltaTime / 2, 0, 0);
+        topWall.localScale += scaleIncrease;
+        bottomWall.localScale += scaleIncrease;
+        leftWall.localScale += scaleIncrease;
+        rightWall.localScale += scaleIncrease;
+    }
+}
