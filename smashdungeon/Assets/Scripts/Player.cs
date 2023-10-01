a296ca76e79fc947e05cf30e52a0eeb60a0349b4
Vitalii <vedmidu4asi@gmail.com>
2023-10-01 09:21:07 -0400

    Health bar and damage

--- smashdungeon/Assets/Scripts/Player.cs
+++ smashdungeon/Assets/Scripts/Player.cs
@@ -14,13 +14,14 @@ class Player: MonoBehaviour {
 
     private void OnMove(InputValue input) {
         var velocity = input.Get<Vector2>();
-        if (velocity != Vector2.zero)
-            GetComponent<Rigidbody2D>().velocity = velocity * speed;
+
+        GetComponent<Rigidbody2D>().velocity = velocity * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Enemy") {
             health -= 10f;
+            Debug.Log("Collision");
         }
     }
 
