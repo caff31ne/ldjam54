51873a38363760c31cb402f765847617ddc2db69
Vitalii <vedmidu4asi@gmail.com>
2023-09-30 20:52:40 -0400

    player change

--- smashdungeon/Assets/Scripts/Player.cs
+++ smashdungeon/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 class Player: MonoBehaviour {
 
     public float speed = 10f;
+    public float health = 100f;
     
     private void Start() {
     }
@@ -15,6 +16,12 @@ class Player: MonoBehaviour {
         var velocity = input.Get<Vector2>();
         if (velocity != Vector2.zero)
             GetComponent<Rigidbody2D>().velocity = velocity * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy") {
+            health -= 10f;
+        }
     }
 
 }
