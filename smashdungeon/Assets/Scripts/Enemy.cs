cd4f1672987f4d4c1098fb6e6e7a028044509f10
humancoded <139584104+humancoded@users.noreply.github.com>
2023-09-30 20:00:09 -0400

    added initial velocity to enemies

--- smashdungeon/Assets/Scripts/Enemy.cs
+++ smashdungeon/Assets/Scripts/Enemy.cs
@@ -1,11 +1,25 @@
+using System;
 using UnityEngine;
 
 class Enemy: MonoBehaviour {
+    
+    public float initialSpeedX = 1.0f;
+    public float initialSpeedY = 1.0f;
 
-    private void Start() {
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _rb.velocity = new Vector2(initialSpeedX, initialSpeedY);
     }
 
     private void Update() {
+        
     }
 
 }
